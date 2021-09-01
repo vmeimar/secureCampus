e28e274115211a50a90c96672bf984f72b7b7e7a
Vaggelis Meimaroglou <vmeimaroglou@uoa.gr>
2021-09-01 12:39:26 +0300

    sync

--- public/js/app.js
+++ public/js/app.js
@@ -49990,8 +49990,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\UOA_WS04\Documents\Code\secureCampus\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\UOA_WS04\Documents\Code\secureCampus\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Code\securecampus\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Code\securecampus\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
